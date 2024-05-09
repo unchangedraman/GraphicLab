e48f2048087054573e5dfaf7a8a0532a29444060
unchangedraman <unchangedraman@gmail.com>
2024-05-09 22:16:40 +0530

    initial init

---
+++ convex-client-provider.tsx
@@ -0,0 +1,6 @@
+
+"use client";
+
+import{ClerkProvider,useAuth} from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://valid-tapir-66.clerk.accounts.dev", // isme issuer wali url chipka do
+        applicationID: "convex", // this need to match with the aud field
+      },
+    ]
+  };
