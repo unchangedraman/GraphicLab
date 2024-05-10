e88e382ebb44e77ed59ca1e25153b71204fc3fd6
unchangedraman <unchangedraman@gmail.com>
2024-05-10 18:14:26 +0530

    designing the components for dashboard

---
+++ components/dashboard/BoardCard/BoardCard.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import Overlay from "./Overlay";
+import { useAuth } from "@clerk/nextjs";
+import { formatDistanceToNow } from "date-fns";
+import Footer from "./Footer";
+import BoardActions from "./BoardActions";
+import { MoreHorizontal } from "lucide-react";
+import { api } from "../../../../convex/_generated/api";
+import { useApiMutation } from "@/hooks/useApiMutation";
+import { toast } from "sonner";
+
+type BoardCardProps = {
+  id: string;
+  title: string;
+  authorName: string;
+  authorId: string;
+  createdAt: number;
+  imageUrl: string;
+  orgId: string;
+  isFavorite: boolean;
+};
+
+const BoardCard = ({
+  id,
+  title,
+  authorName,
+  authorId,
+  createdAt,
+  imageUrl,
+  orgId,
+  isFavorite,
+}: BoardCardProps) => {
+  const { userId } = useAuth();
+
+  const authorLabel = userId === authorId ? "You" : authorName;
+  const createdAtLabel = formatDistanceToNow(createdAt, {
+    addSuffix: true,
+  });
+
+  const { mutate: onFavorite, pending: pendingFavorite } = useApiMutation(
+    api.board.favorite
+  );
+  const { mutate: onUnfavorite, pending: pendingUnfavorite } = useApiMutation(
+    api.board.unfavorite
+  );
+
+  const toggleFavorite = () => {
+    if (isFavorite) {
+      onUnfavorite({ id }).catch(() => toast.error("Failed to unfavorite"));
+    } else {
+      onFavorite({ id, orgId }).catch(() => toast.error("Failed to favorite"));
+    }
+  };
+
+  return (
+    <Link href={`/board/${id}`}>
+      <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
+        <div className="relative flex-1 bg-amber-50">
+          <Image src={imageUrl} alt={title} fill className="object-fit" />
+          <Overlay />
+          <BoardActions id={id} title={title} side="right">
+            <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+              <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity" />
+            </button>
+          </BoardActions>
+        </div>
+        <Footer
+          isFavorite={isFavorite}
+          title={title}
+          authorLabel={authorLabel}
+          createdAtLabel={createdAtLabel}
+          onClick={toggleFavorite}
+          disabled={pendingFavorite || pendingUnfavorite}
+        />
+      </div>
+    </Link>
+  );
+};
+
+export default BoardCard;
